4254e8aeab2273fd1fa8462a6a7ee149beb22ae5
hernandez <natali.mooreno@gmail.com>
2022-12-17 12:58:03 -0500

    Semana 2: se adicionó controllers y DAO

---
+++ Semana_2/Sist_Fact_G7_Grupo17 S2/src/main/java/com/unab/app/dao/IFacturaDAO.java
@@ -0,0 +1,15 @@
+package com.unab.app.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unab.app.models.Factura;
+
+@Repository
+public interface IFacturaDAO extends CrudRepository<Factura, Long> {
+	@Query("select f from Factura f join fetch f.cliente c where f.cliente.id =?1")
+	public List<Factura> fetchByIdClienteWithFactura(Long id);
+}
